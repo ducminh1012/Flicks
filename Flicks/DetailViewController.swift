c77278d80c8d265589e0b0d73fee82d34f9e45af
ducminh1012 <ducminh1012@gmail.com>
2016-10-16 16:29:16 +0700

    Add animation from low to high resolution image poster

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -26,8 +26,44 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let posterUrl = "\(data!.originalPath)\(data!.imagePath)"
+        let lowUrl = "\(data!.lowPath)\(data!.imagePath)"
         
-        posterImageView.setImageWith(URL(string: posterUrl)!, placeholderImage: UIImage(named: "movie_placeholder"))
+        
+        let highRequest = URLRequest(url: URL(string: posterUrl)!)
+        let lowRequest = URLRequest(url: URL(string: lowUrl)!)
+        
+        posterImageView.setImageWith(lowRequest, placeholderImage: nil, success: { (imageRequest, imageRespone, image) in
+            
+                self.posterImageView.alpha = 0.0
+                self.posterImageView.image = image
+            
+            UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                
+                self.posterImageView.alpha = 1.0
+                
+                }, completion: { (sucess) -> Void in
+                    
+                    // The AFNetworking ImageView Category only allows one request to be sent at a time
+                    // per ImageView. This code must be in the completion block.
+                    self.posterImageView.setImageWith(
+                        highRequest,
+                        placeholderImage: image,
+                        success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                            
+                            self.posterImageView.image = largeImage
+                            
+                        },
+                        failure: { (request, response, error) -> Void in
+                            // do something for the failure condition of the large image request
+                            // possibly setting the ImageView's image to a default image
+                    })
+            })
+            
+            
+            }) { (imageRequest, imageRespone, error) in
+                print(error)
+        }
+
         
         titleLabel.text = data?.name
         overviewLabel.text = data?.overview
